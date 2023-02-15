6e574a5173a3e66acdb0bc142778a92a94cf049f
Aaron Moon <112985603+aemoon03@users.noreply.github.com>
2023-02-15 15:29:40 -0800

    HELP ME

--- grade.sh
+++ grade.sh
@@ -3,3 +3,27 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission 
+
+if [[ -f ListExamples.java ]]
+then
+    echo "ListExamples found"
+else
+    echo "need file ListExamples.java"
+    exit 1
+fi 
+
+cp ../TestListExamples.java ./ 
+
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "successful compile"
+else    
+    echo "failed to compile"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
